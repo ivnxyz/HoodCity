34bdb3a994369455f4c8c07c6f65c949c2d01d05
Iván Martínez <ivanmartinez33@outlook.com>
2017-07-11 20:33:30 -0500

    Project created

---
+++ HoodCity/HoodCity/HoodCity-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  HoodCity-Bridging-Header.h
+//  HoodCity
+//
+//  Created by Iván Martínez on 11/07/17.
+//  Copyright © 2017 Iván Martínez. All rights reserved.
+//
+
+#import "GeoFire.h"
---
+++ HoodCity/Pods/Target Support Files/Pods-HoodCity/Pods-HoodCity-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HoodCity : NSObject
+@end
+@implementation PodsDummy_Pods_HoodCity
+@end
