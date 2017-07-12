0784025ee1472638fe17a3013ab86973081ae1ae
Iván Martínez <ivanmartinez33@outlook.com>
2017-07-11 21:41:23 -0500

    Getting user's location

--- HoodCity/HoodCity/HomeController.swift
+++ HoodCity/HoodCity/HomeController.swift
@@ -13,18 +13,40 @@ class HomeController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    lazy var locationManager: LocationManager = {
+        return LocationManager(mapView: self.mapView)
+    }()
+    
+    var mapHasCenteredOnce = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        mapView.userTrackingMode = .follow
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        locationManager.getAuthStatus()
     }
 
     @IBAction func newEvent(_ sender: UIButton) {
+        
     }
 
 }
 
+extension HomeController: MKMapViewDelegate {
+    
+    // MARK: - MKMapViewDelegate
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        guard let location = userLocation.location else { return }
+        
+        if !mapHasCenteredOnce {
+            locationManager.centerMapOnLocation(location)
+            mapHasCenteredOnce = true
+        }
+    }
+    
+}
