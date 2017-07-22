1ac40c55221df4f85778f7f8cd301f440f21de03
Iván Martínez <ivanmartinez33@outlook.com>
2017-07-22 02:41:17 -0500

    GADMobileAds configured

--- HoodCity/HoodCity/AppDelegate.swift
+++ HoodCity/HoodCity/AppDelegate.swift
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         FirebaseApp.configure()
+        GADMobileAds.configure(withApplicationID: addMobAppId)
         
         return true
     }
---
+++ HoodCity/HoodCity/AppId.swift
@@ -0,0 +1,11 @@
+//
+//  AppID.swift
+//  HoodCity
+//
+//  Created by Iván Martínez on 22/07/17.
+//  Copyright © 2017 Iván Martínez. All rights reserved.
+//
+
+import Foundation
+
+let addMobAppId = "your app id here"
